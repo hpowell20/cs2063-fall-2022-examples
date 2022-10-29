3c498d9e3efba091765368b97d612f9a75048892
Howard Powell <hspowell@gmail.com>
2022-10-29 12:20:33 -0300

    Lecture 7 examples

--- lab-solutions/Lab4/BroadcastReceiverSkeleton/app/src/main/java/mobiledev/unb/ca/lab4skeleton/Constants.kt
+++
@@ -1,7 +0,0 @@
-package mobiledev.unb.ca.lab4skeleton
-
-object Constants {
-    // Strings will serve as keys when saving state between activities
-    const val NOTIFICATION_CHANNEL_ID = "mobiledev.unb.ca.lab4skeleton.channel_01"
-}
-
